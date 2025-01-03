15969bb818ccc81d0a17dc62f2f8eb3df8dd26f9
Keneil-Samms <keneil@innovate10x.com>
2025-01-03 14:08:00 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//to Garfeild trim console.log($message.trim()); on line 6
+//Eunice print message using console.log(); on line 6
+message = "Hello World";
+function Print_Hello_World(message){
+//implement here
+
+}
